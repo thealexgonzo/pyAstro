a39f8d2ec7637f4c21a21424e93038e83ef00f4a
Alejandro González <alexgonzorod@hotmail.com>
2026-02-05 09:54:11 +0000

    implemented player rotation

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-    
+
 
 if __name__ == "__main__":
     main()
