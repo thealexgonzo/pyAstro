d5b94cf98bff0ef1953ae95de2de12524f004371
Alejandro González <alexgonzorod@hotmail.com>
2026-02-05 09:33:26 +0000

    Incorporating game loop, logging game state and adding sprites

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
